e965dbfe06a36b3cc382a075a797ffa870cf2ec1
abhijeet <redefinedabhi2@gmail.com>
2022-10-23 17:11:08 +0530

    adding conectivity livedata in mainactivity

--- app/src/main/java/com/theleafapps/pro/networkcheck/MainActivity.kt
+++ app/src/main/java/com/theleafapps/pro/networkcheck/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var connectivityLiveData:ConnectivityLiveData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        connectivityLiveData= ConnectivityLiveData(application)
+
+
     }
 }
